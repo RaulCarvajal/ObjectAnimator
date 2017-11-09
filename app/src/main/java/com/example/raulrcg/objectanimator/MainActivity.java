d228f628b794447b6fd69254fa56beb80b1ff681
Raúl Rafael Carvajal Gutierrez <raracarvajal@ittepic.edu.mx>
2017-11-09 07:53:22 -0700

    actualizado

--- app/src/main/java/com/example/raulrcg/objectanimator/MainActivity.java
+++ app/src/main/java/com/example/raulrcg/objectanimator/MainActivity.java
@@ -4,7 +4,11 @@ import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -30,6 +34,41 @@ public class MainActivity extends AppCompatActivity {
 //        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "y", 0, targetY).setDuration(1000);
         ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "y", targetY,2).setDuration(1000);
         animator.setInterpolator(new BounceInterpolator());
+        animator.start();
+    }
+
+    public void onButtonClick1(View v) {
+
+        int screenHeight = canvas.getHeight();
+        int targetY = screenHeight - imageView.getHeight();
+
+//        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "y", 0, targetY).setDuration(1000);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "y", targetY,2).setDuration(1000);
+        //animator.setInterpolator(new BounceInterpolator());
+        animator.setInterpolator(new AccelerateInterpolator());
+        animator.start();
+    }
+
+    public void onButtonClick2(View v) {
+
+        int screenHeight = canvas.getHeight();
+        int targetY = screenHeight - imageView.getHeight();
+
+//        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "y", 0, targetY).setDuration(1000);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "y", targetY,2).setDuration(1000);
+        //animator.setInterpolator(new BounceInterpolator());
+        animator.setInterpolator(new CycleInterpolator(2));
+        animator.start();
+    }
+    public void onButtonClick3(View v) {
+
+        int screenHeight = canvas.getHeight();
+        int targetY = screenHeight - imageView.getHeight();
+
+//        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "y", 0, targetY).setDuration(1000);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "y", targetY,1).setDuration(1000);
+        //animator.setInterpolator(new BounceInterpolator());
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
         animator.start();
     }
 }
